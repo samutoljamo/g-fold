ba3aff200e04fbdfea478f0566ece78b0b91c470
Samu Toljamo <samu.toljamo@gmail.com>
2021-11-14 18:45:59 +0200

    install script fixed and tested manually

--- install.py
+++ install.py
@@ -3,16 +3,16 @@ import sys
 
 
 
-def install(*args):
-    pip = sys.executable + " -m pip"
-    subprocess.run([pip, "install", ",".join(args)])
+def install(*args, deps=True):
+    arguments = [sys.executable, "-m", "pip", "install", ",".join(args)]
+    if not deps:
+        arguments.append("--no-deps")
+    subprocess.run(arguments)
 
 if __name__ == '__main__':
     install("wheel") # wheel first
-    install("git+https://github.com/moehle/cvxpy_codegen.git --no-deps") # dependencies are broken
-    print("installed wheel")
+    install("git+https://github.com/moehle/cvxpy_codegen.git", deps=False) # dependencies are broken
     with open("requirements.txt", "r") as file:
         modules = file.read().splitlines()
         for mod in modules:
             install(mod)
-            print("installed", mod)
